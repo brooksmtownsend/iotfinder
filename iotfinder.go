3390d7fc2b1045f47455af5d2917a20d432413ff
Brooks Townsend <brooksmtownsend@gmail.com>
2021-03-04 16:05:30 -0500

    cleaned up code

--- iotfinder.go
+++ iotfinder.go
@@ -11,27 +11,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Discovering local addresses ...")
+	// regular expression for an ipv4 address
+	reIpv4 := regexp.MustCompile(`\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b`)
 
+	fmt.Println("Discovering local addresses ...")
 	out, err := exec.Command("arp", "-a").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// regular expression for an ipv4 address
-	reIpv4 := regexp.MustCompile(`[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+`)
-
 	fmt.Println("Testing ssh connectivity ...")
 	for _, addr := range strings.Split(string(out), "\n") {
-		//TODO(brooksmtownsend): append port to ip address, dial
 		ip := string(reIpv4.Find([]byte(addr)))
 		s, _ := time.ParseDuration("1s")
-		_, err = net.DialTimeout("tcp4", ip+"22", s)
-		if err == nil {
+		if _, err := net.DialTimeout("tcp4", ip+":22", s); err == nil {
 			log.Printf("connected to %s at port 22", ip)
 		}
 	}
-
 }
-
-// tcpAddr := net.TCPAddr{IP: net.ParseIP(string(reIpv4.Find([]byte(addr)))), Port: 22}
