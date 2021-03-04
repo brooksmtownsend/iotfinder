ddcfe29e667f9fed7aee71e4c49afc34967a5962
Brooks Townsend <brooksmtownsend@gmail.com>
2021-03-04 16:59:54 -0500

    optimized discovery with goroutines

--- iotfinder.go
+++ iotfinder.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -14,18 +15,27 @@ func main() {
 	// regular expression for an ipv4 address
 	reIpv4 := regexp.MustCompile(`\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b`)
 
+	// use arp to find local addresses
 	fmt.Println("Discovering local addresses ...")
 	out, err := exec.Command("arp", "-a").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// see which addresses are open on port 22
 	fmt.Println("Testing ssh connectivity ...")
+	var wg sync.WaitGroup
 	for _, addr := range strings.Split(string(out), "\n") {
 		ip := string(reIpv4.Find([]byte(addr)))
-		s, _ := time.ParseDuration("1s")
-		if _, err := net.DialTimeout("tcp4", ip+":22", s); err == nil {
-			log.Printf("connected to %s at port 22", ip)
-		}
+		wg.Add(1)
+		go func(ip string) {
+			defer func() {
+				wg.Done()
+			}()
+			if _, err := net.DialTimeout("tcp4", ip+":22", 1*time.Second); err == nil {
+				log.Printf("connected to %s at port 22", ip)
+			}
+		}(ip)
 	}
+	wg.Wait()
 }
